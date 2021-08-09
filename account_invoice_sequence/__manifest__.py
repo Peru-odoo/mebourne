9ffa2916abad7382f4c30bffc6ab0bd67854ea3d
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-09 11:38:37 +0630

    modified account invoice sequence

---
+++ account_invoice_sequence/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Invoice Sequence",
+
+    'summary': """Customer Invoice Sequence""",
+
+    'description': """
+        Customer Invoice Sequence By Branch.
+    """,
+
+    'author': "asiamatrix",
+
+    'website': "http://www.asiamatrixsoftware.com",
+
+    'category': 'Accounting',
+
+    'version': '0.1',
+
+    'depends': [
+        'account',
+        'multi_branches',
+    ],
+
+    'data': [
+        'views/branches_sequence.xml',
+    ],
+}
