eec3355dbbf8154523fe4df1582e607347ed6be1
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-10 16:33:20 +0630

    modified sale

--- multi_branches_sequence/model/sale.py
+++ multi_branches_sequence/model/sale.py
@@ -3,9 +3,9 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    @api.model
     def create(self, vals):
-        branch = vals.get("branch_id", False)
-        if branch:
+        if self.branch_id:
             sequence_id = self.env['res.branch'].browse(vals['branch_id']).so_sequence_id
             vals["name"] = sequence_id._next() or "/"
         return super(SaleOrder, self).create(vals)
