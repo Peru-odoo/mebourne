a54f8a4aae82626c8ab6c4ddcf7a4f5a6ce3579b
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-10 16:55:39 +0630

    modified purchase order form

--- multi_branches_sequence/model/purchase.py
+++ multi_branches_sequence/model/purchase.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class PurchaseOrder(models.Model):
@@ -9,7 +10,10 @@ class PurchaseOrder(models.Model):
         branch = vals.get("branch_id", False)
         if branch:
             sequence_id = self.env['res.branch'].browse(branch).po_sequence_id
-            vals["name"] = sequence_id._next() or "/"
+            if sequence_id:
+                vals["name"] = sequence_id._next() or "/"
+            else:
+                raise UserError(_("Please define a sequence on your branch."))
         return super(PurchaseOrder, self).create(vals)
 
 
