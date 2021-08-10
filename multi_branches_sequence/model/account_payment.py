8f2e6687737f10b56be8df8164599a98e30ffaf8
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-10 17:17:58 +0630

    modified payment form 1

--- multi_branches_sequence/model/account_payment.py
+++ multi_branches_sequence/model/account_payment.py
@@ -11,17 +11,11 @@ class account_payment(models.Model):
             branch = rec.branch_id
             if rec.partner_type == 'customer':
                 if rec.payment_type == 'inbound':
-                    sequence_id = self.env['res.branch'].browse(branch).cus_sequence_id
-                    if sequence_id:
-                        rec.name = sequence_id._next()
-                    else:
-                        raise UserError(_("Please define a sequence on your branch."))
-
+                    sequence_id = branch.cus_sequence_id
+                    rec.name = sequence_id
             if rec.partner_type == 'supplier':
                 if rec.payment_type == 'inbound':
-                    sequence_id = self.env['res.branch'].browse(branch).ven_sequence_id
-                    if sequence_id:
-                        rec.name = sequence_id._next()
-                    else:
-                        raise UserError(_("Please define a sequence on your branch."))
+                    sequence_id = branch.ven_sequence_id
+                    rec.name = sequence_id
+
         return res
