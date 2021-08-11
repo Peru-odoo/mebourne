511d2e7b73061a7502c17c3766712a282580a143
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-11 13:35:56 +0630

    modified account

--- multi_branches_sequence/model/account_move_extend.py
+++ multi_branches_sequence/model/account_move_extend.py
@@ -10,6 +10,10 @@ class AccountMove(models.Model):
         branch = self.branch_id
         if self.type in 'out_invoice' or not journal.refund_sequence:
             return branch.inv_sequence_id
+        if self.type in 'out_refund' or not journal.refund_sequence:
+            return branch.credit_sequence_id
         if self.type in 'in_invoice' or not journal.refund_sequence:
             return branch.bill_sequence_id
+        if self.type in 'in_refund' or not journal.refund_sequence:
+            return branch.refund_sequence_id
         return res
