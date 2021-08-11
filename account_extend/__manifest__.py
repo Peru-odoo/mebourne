ef56194dc0a8d17061ca5a3db7d5fa4c9ce9d3f3
pyaepyae@asiamatrixsoftware.com <pyaepyae@asiamatrixsoftware.com>
2021-08-11 11:08:26 +0630

    modified account extend

---
+++ account_extend/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Accounting Extend",
+
+    'summary': """
+        Add Total Payment Amount in invoice list view""",
+
+    'description': """
+        Add Total Payment Amount in invoice list view
+    """,
+
+    'author': "znl",
+    'website': "http://www.asiamatrixsoftware.com",
+    'category': 'Accounting',
+    'version': '0.3',
+    'depends': ['account'],
+    'data': [
+
+        'views/account_move_view.xml',
+
+    ],
+}
